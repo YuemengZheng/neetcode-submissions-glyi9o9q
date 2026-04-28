2c729ef536641ea189eff8ec5afb4cc022b074e2
YuemengZheng <zheng.yuem@northeastern.edu>
2026-04-28 15:57:40 -0700

    Add: redundant-connection - submission-11

---
+++ Data Structures & Algorithms/redundant-connection/submission-11.java
@@ -0,0 +1,60 @@
+class Solution {
+    class UnionFind{
+        int[] parent;
+        int[] size;
+
+        public UnionFind(int n){
+            parent = new int[n + 1];
+            size = new int[n + 1];
+
+            for(int i = 1; i<= n; i++){
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        public int find(int x){
+            if(x != parent[x]){
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        public boolean union(int a, int b){
+            int rootA = find(a);
+            int rootB = find(b);
+            if(rootA == rootB){
+                return false;
+            }
+
+            if(size[rootA] < size[rootB]){
+                parent[rootA] = rootB;
+                size[rootB] += size[rootA];
+            }else{
+                parent[rootB] = rootA;
+                size[rootA] += size[rootB];
+            }
+            return true;
+        }
+    }
+    public int[] findRedundantConnection(int[][] edges) {
+        // We use Union Find to detect the edge that creates a cycle.
+        // Initially, each node is in its own set.
+        // We iterate through each edge.
+        // If the two nodes are already in the same set, adding this edge will create a cycle, so we return it.
+        // Otherwise, we union the two nodes.
+        // *********TC: O(n), SC: O(n)*********
+
+        UnionFind uf = new UnionFind(edges.length);
+        for(int[] edge : edges){
+            int a = edge[0];
+            int b = edge[1];
+
+            if(!uf.union(a, b)){
+                return new int[]{a, b};
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
+   
