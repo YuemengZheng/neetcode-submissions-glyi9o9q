31870aba05053a43cf0f501fd10cbef297782e8b
YuemengZheng <zheng.yuem@northeastern.edu>
2026-05-03 08:18:00 -0700

    Add: subsets - submission-4

---
+++ Data Structures & Algorithms/subsets/submission-4.java
@@ -0,0 +1,17 @@
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+    public List<List<Integer>> subsets(int[] nums) {
+        backtrack(0, nums);
+        return res;
+    }
+    private void backtrack(int start, int[] nums){
+        res.add(new ArrayList<>(path));
+
+        for(int i = start; i < nums.length; i++){
+            path.add(nums[i]);
+            backtrack(i + 1, nums);// 从i + 1开始！！！！
+            path.remove(path.size() - 1);
+        }
+    }
+}
