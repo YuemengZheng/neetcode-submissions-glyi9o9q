9fc6122ce38c159a7662f5d8b2d9c187b9aceb50
YuemengZheng <zheng.yuem@northeastern.edu>
2026-05-14 16:15:02 -0700

    Add: search-2d-matrix - submission-5

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-5.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // ***** TIME: O(log(n * m)), SPACE:O(1) *****
+        // We treat the 2D matrix as a sorted 1D array and apply binary search.
+        // We compare the middle element with the target
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int left = 0, right = n * m  - 1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            int x = mid / m;
+            int y = mid % m;
+            if(matrix[x][y] == target){
+                return true;
+            }else if(matrix[x][y] < target){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+}
