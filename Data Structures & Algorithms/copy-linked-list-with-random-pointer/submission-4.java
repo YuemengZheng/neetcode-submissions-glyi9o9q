28653a8ad088ccda38d149fb6fa541aa115b8db1
YuemengZheng <zheng.yuem@northeastern.edu>
2026-05-15 13:36:51 -0700

    Add: copy-linked-list-with-random-pointer - submission-4

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-4.java
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        Map<Node, Node> map = new HashMap<>();
+        return dfs(head, map);
+    }
+    private Node dfs(Node head, Map<Node, Node> map){
+        if(head == null){
+            return null;
+        }
+        if(map.containsKey(head)){
+            return map.get(head);
+        }
+
+        Node copy = new Node(head.val);
+        map.put(head, copy);
+        copy.next = dfs(head.next, map);
+        copy.random = dfs(head.random, map);
+        
+        return copy;
+    }
+}
