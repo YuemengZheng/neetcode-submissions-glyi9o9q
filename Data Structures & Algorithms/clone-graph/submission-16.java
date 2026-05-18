6e829bdbd8dfefe67e8d795c05d137d13eecb483
YuemengZheng <zheng.yuem@northeastern.edu>
2026-05-18 16:43:00 -0700

    Add: clone-graph - submission-16

---
+++ Data Structures & Algorithms/clone-graph/submission-16.java
@@ -0,0 +1,41 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        // recursion
+        Map<Node, Node> map = new HashMap<>();
+        return dfs(node, map);
+    }
+    private Node dfs(Node node, Map<Node, Node> map){
+        if(node == null) return null;
+        if(map.containsKey(node)) return map.get(node);
+
+        Node clone = new Node(node.val);
+        clone.neighbors = new ArrayList<>();
+        map.put(node, clone);
+        
+        for(Node next : node.neighbors){
+            clone.neighbors.add(dfs(next, map));
+        }
+
+        return clone;
+    }
+}
