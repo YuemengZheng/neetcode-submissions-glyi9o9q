7566b17da32dc6ef66bfec2a3f745e1fb50481df
YuemengZheng <zheng.yuem@northeastern.edu>
2026-05-21 13:07:06 -0700

    Add: top-k-elements-in-list - submission-11

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-11.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        /*
+        get each number's frequency in the nums
+        use k sized 最小堆，最终得到top k frequent elements
+        nlogk| n
+        */
+        Map<Integer, Integer> cnts = new HashMap<>();
+        for(int num : nums){
+            cnts.put(num, cnts.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));// comparator
+        for(Map.Entry<Integer, Integer> entry : cnts.entrySet()){
+            minHeap.offer(new int[]{entry.getKey(), entry.getValue()});
+            if(minHeap.size() > k){
+                minHeap.poll();
+            }
+        }
+        int[] res = new int[k];
+        for(int i = 0; i < k; i++){
+            res[i] = minHeap.poll()[0];
+        }
+        return res;
+    }
+}
