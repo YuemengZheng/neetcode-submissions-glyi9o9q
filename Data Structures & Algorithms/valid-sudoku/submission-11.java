71c19a883fcd947e5b55f41a87b79448ada78577
YuemengZheng <zheng.yuem@northeastern.edu>
2026-05-23 20:30:46 -0700

    Add: valid-sudoku - submission-11

---
+++ Data Structures & Algorithms/valid-sudoku/submission-11.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        /* 
+        The idea is to scan the board and check whether a digit has already appeared 
+        in the same row, column, or subbox.
+
+        To track this efficiently, we use three boolean matrix to record 
+        whether a digit has been seen in each row, column, and box.
+        If we encounter a digit that has already appeared in any of these, the board is invalid.
+        */
+        // ***** TIME: O(1), SPACE:O(1) *****
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                char c = board[i][j];
+                if(c == '.'){
+                    continue;
+                }
+                int num = c - '1';// convert the digit from character to an index from 0 to 8
+                int box = (i / 3) * 3 + j / 3; // compute which box the cell belongs to
+                if(rows[i][num] || cols[j][num] || boxes[box][num]){
+                    return false;
+                }
+                rows[i][num] = true;
+                cols[j][num] = true;
+                boxes[box][num] = true;
+            }
+        }
+        return true;
+    }
+}
+
+
