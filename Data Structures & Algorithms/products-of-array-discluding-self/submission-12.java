0ac1066b11525d2b76fe5d12d322b66a4f11e8cb
YuemengZheng <zheng.yuem@northeastern.edu>
2026-05-23 19:41:58 -0700

    Add: products-of-array-discluding-self - submission-12

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-12.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        // 用prefix和surfix去处理得到左边的乘积和右边的乘积
+        // 最终把他们乘在一起就行了
+        // 我们可以优化只用一个array先处理prefix，再处理surfix
+        // int[] prefix = new int[nums.length];
+        // prefix[0] = 1;
+        // for(int i = 1; i < nums.length; i++){
+        //     prefix[i] = prefix[i - 1] * nums[i - 1];
+        // }
+        // int[] surfix = new int[nums.length];
+        // surfix[nums.length - 1] = 1;
+        // for(int i = nums.length - 2; i >= 0; i--){
+        //     surfix[i] = surfix[i + 1] * nums[i + 1];
+        // }
+        // for(int i = 0; i < nums.length; i++){
+        //     prefix[i] *= surfix[i];
+        // }
+        // return prefix;
+        int[] res = new int[nums.length];
+        res[0] = 1;
+        for(int i = 1; i < nums.length; i++){
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        int surfix = 1;
+        for(int i = nums.length - 1; i >= 0; i--){
+            res[i] *= surfix;
+            surfix *= nums[i];
+        }
+        return res;
+    }
+}  
