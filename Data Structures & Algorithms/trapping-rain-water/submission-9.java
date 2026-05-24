f201d52d932cdab71db73f2d09bb012f0cfc46ff
YuemengZheng <zheng.yuem@northeastern.edu>
2026-05-24 12:09:01 -0700

    Add: trapping-rain-water - submission-9

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-9.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int trap(int[] height) {
+        int res = 0;
+        int leftMax = 0;
+        int rightMax = 0;
+        int l = 0;
+        int r = height.length - 1;
+        while(l <= r){
+            if(leftMax < rightMax){
+                res += Math.max(leftMax - height[l], 0);
+                leftMax = Math.max(leftMax, height[l++]);
+            }else{
+                res += Math.max(rightMax - height[r], 0);
+                rightMax = Math.max(rightMax, height[r--]);
+            }
+        }
+        return res;
+    }
+}
