06aecbbccc630d50beb9f3b38b4c74868268c67e
YuemengZheng <zheng.yuem@northeastern.edu>
2026-06-11 21:35:31 -0700

    Add: longest-common-prefix - submission-6

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-6.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        /*
+        For each index i, check if all strings have the same character at that position. 
+        If not, return the prefix up to i
+        */
+        // t: m * n  s: n
+        String common = strs[0];
+        for(int i = 0; i < common.length(); i++){
+            for(String s : strs){
+                if(i >= s.length() || common.charAt(i) != s.charAt(i)){
+                    return common.substring(0, i);
+                }
+            }
+        }
+        return common;
+    }
+}
