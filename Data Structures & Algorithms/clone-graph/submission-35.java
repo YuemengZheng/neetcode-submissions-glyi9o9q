5cab2beaeb1322ea830972f66b5fbf35d66681c4
YuemengZheng <zheng.yuem@northeastern.edu>
2026-06-12 20:01:36 -0700

    Add: clone-graph - submission-35

---
+++ Data Structures & Algorithms/clone-graph/submission-35.java
@@ -0,0 +1,46 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        // use bfs
+        // for each node, build it's neighbors
+        if(node == null) return null;
+        Map<Node, Node> map = new HashMap<>();
+        Queue<Node> q = new ArrayDeque<>();
+        Node cur = node;
+        q.offer(cur);
+        map.put(cur, new Node(cur.val));
+        
+        while(!q.isEmpty()){
+            cur = q.poll();
+            if(cur.neighbors == null) continue;
+            for(Node next : cur.neighbors){
+                if(!map.containsKey(next)){
+                    q.offer(next);
+                    map.put(next, new Node(next.val));
+                }
+                map.get(cur).neighbors.add(map.get(next));
+            }
+        }
+        return map.get(node);
+    }
+      
+}
