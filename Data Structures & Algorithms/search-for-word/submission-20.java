f11f17b577d646e110c7db5a42c3c01e2ca65dd9
YuemengZheng <zheng.yuem@northeastern.edu>
2026-06-23 22:03:28 -0700

    Add: search-for-word - submission-20

---
+++ Data Structures & Algorithms/search-for-word/submission-20.java
@@ -0,0 +1,57 @@
+class Solution {
+    int[][] DIRS = new int[][]{{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+    boolean[][] visited;
+    public boolean exist(char[][] board, String word) {
+        /*
+        I'll use backtracking. 
+        For each cell in the board, I treat it as the starting point. 
+        At each step, I check if the current character matches, if so try 4 directions. 
+        If all characters match, return true.
+
+        Time is O(m * n * 4^L), space is O(L) for the recursion stack.
+        */
+        int m = board.length;
+        int n = board[0].length;
+        visited = new boolean[m][n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(backtrack(i, j, 0, word, board)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private boolean backtrack(int i, int j, int index, String word, char[][] board){
+        if(index == word.length()){
+            return true;
+        }
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length 
+        || visited[i][j] || board[i][j] != word.charAt(index)){
+            return false;
+        }
+
+        visited[i][j] = true;
+        for(int[] dir : DIRS){
+            int x = dir[0] + i;
+            int y = dir[1] + j;
+            /*
+            1. 把边界的检查写在这会有一个问题，当我们已经找到了word，但是index + 1是越界的，我们就没办法触及base case return true
+            所以把边界check写在 index == word.length()这个base case的下面，确保都能有机会check到！！！
+
+            2. 当backtrack有返回值的时候我们应该怎么返回
+            if(x >= 0 && x < board.length && y >= 0 && y < board[0].length && !visited[x][y]){
+                if(backtrack(x, y, index + 1, word, board)){
+                    return true;
+                }
+            }
+            */
+            if(backtrack(x, y, index + 1, word, board)){
+                return true;// 找到了立刻返回
+            }
+
+        }
+        visited[i][j] = false;
+        return false;// 都没找到的返回false
+    }
+}
