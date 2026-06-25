d22675296613807267711449c9c0eb55be87ff2a
YuemengZheng <zheng.yuem@northeastern.edu>
2026-06-24 20:34:10 -0700

    Add: sort-colors - submission-3

---
+++ Data Structures & Algorithms/sort-colors/submission-3.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        // three pointers
+        if(nums.length <= 1) return;
+        int p0 = 0;
+        int p2 = nums.length - 1;
+        int cur = 0;
+        while(cur <= p2){
+        // three caeses:
+        // if cur = 1, move cur forward
+            if(nums[cur] == 1){
+                cur++;
+            }
+        // if cur = 0, swap p0 with cur, move both right
+            else if(nums[cur] == 0){
+                swap(cur, p0, nums);
+                cur++;
+                p0++;
+            }
+        // if cur = 2, swap it with p2, move p2 left
+            else{
+                swap(cur, p2, nums);
+                p2--;
+            }
+        }
+    }
+    private void swap(int i, int j, int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
+/**/
