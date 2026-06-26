7cb1e7112c1fa702b598778befe4b0917c5942ae
YuemengZheng <zheng.yuem@northeastern.edu>
2026-06-26 11:05:58 -0700

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        // use two pointers starting from both strings, take one char from them alternatively to build the new string
+        int i = 0;
+        int j = 0;
+        StringBuilder sb = new StringBuilder();
+        while(i < word1.length() && j < word2.length()){
+            sb.append(word1.charAt(i++)).append(word2.charAt(j++));
+        }
+        if(i < word1.length()){
+            sb.append(word1.substring(i));
+        }
+        if(j < word2.length()){
+            sb.append(word2.substring(i));
+        }
+        return sb.toString();
+    }
+}
