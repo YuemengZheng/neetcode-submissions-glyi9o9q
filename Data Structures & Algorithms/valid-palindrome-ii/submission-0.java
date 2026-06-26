5e00f1dc74c90682e7852c1ce62dfa1279f845ac
YuemengZheng <zheng.yuem@northeastern.edu>
2026-06-26 10:53:24 -0700

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        // use two pointers to check from both ends
+        // when we first meet not match, we try to skip the left element or right element and check if one of them can be palindrome
+        // if so return true, else return false
+        int l = 0; int r = s.length() - 1;
+        boolean used = false;
+        while(l < r) {
+            if(s.charAt(l) == s.charAt(r)) {
+                l++;
+                r--;
+            } else{
+                if(used) return false;
+                else {
+                    return isPalindrome(l + 1, r, s) || isPalindrome(l, r - 1, s);
+                }
+            }
+        }
+        return true; 
+    }
+    private boolean isPalindrome(int l, int r, String s) {
+        while(l < r){
+            if(s.charAt(l) == s.charAt(r)) {
+                l++;
+                r--;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
