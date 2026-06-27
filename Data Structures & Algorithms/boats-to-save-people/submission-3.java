5cefc0ae77efbe2c9b5cdd3d40225dfcbdb03ebc
YuemengZheng <zheng.yuem@northeastern.edu>
2026-06-26 18:11:20 -0700

    Add: boats-to-save-people - submission-3

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-3.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        // sort the arr
+        // use two pointers starting from both ends
+        // if sum <= limit cnt++， move both
+        // else cnt++, move right to left
+
+        // nlogn 
+        Arrays.sort(people);
+        int cnt = 0;
+        int l = 0;
+        int r = people.length - 1;
+        while(l <= r){
+            // int sum = l < r ? people[l] + people[r] : people[l];
+            // 1 <= people[i] <= limit
+            int sum = people[r] + people[l];
+            if(sum <= limit){
+                cnt++;
+                l++;
+                r--;
+            }else{
+                cnt++;
+                r--;
+            }
+        }
+        return cnt;
+    }
+}
