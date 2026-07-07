aa67294c1968ff5255492f55955110374e8ababe
YuemengZheng <zheng.yuem@northeastern.edu>
2026-07-07 07:58:17 -0700

    Add: combinations - submission-2

---
+++ Data Structures & Algorithms/combinations/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+
+    public List<List<Integer>> combine(int n, int k) {
+        // choose k elements from the range[1, n]
+        // helper function: takes n, k, startNum
+        backtracking(n, k, 1);
+        return res;       
+    }
+    private void backtracking(int n, int k, int startNum){
+        if(path.size() == k){
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        // if(startNum > n){
+        //     return;
+        // }
+
+        // I iterate from startNum to the last possible starting index that still allows us 
+        // to pick enough elements to fill the path to size k.
+        for(int i = startNum; i <= n - (k - path.size()) + 1; i++){ 
+            path.add(i);
+            backtracking(n, k, i + 1);
+            path.remove(path.size() - 1);
+        }
+    }
+}
