4e5a2f46cc57f13b9b5f9e068782eb0db6827868
YuemengZheng <zheng.yuem@northeastern.edu>
2026-07-07 17:39:22 -0700

    Add: sum-of-all-subset-xor-totals - submission-5

---
+++ Data Structures & Algorithms/sum-of-all-subset-xor-totals/submission-5.java
@@ -0,0 +1,29 @@
+class Solution {
+    int res = 0;
+    public int subsetXORSum(int[] nums) {
+        // backtracking
+        // gloabl variable => res
+        // helper function: inputs => nusm, startindex, xor
+        // for each step: 
+        // res += xor
+        // iterate through the arr starts from the startidnex, try to xor the element, recurse
+        // return res  
+        backtracking(nums, 0, 0);
+        return res;
+    }
+    private void backtracking(int[] nums, int start, int xor){
+        res += xor;
+        for(int i = start; i < nums.length; i++){
+            xor ^= nums[i];
+            backtracking(nums, i + 1, xor);
+            xor ^= nums[i];
+        }
+    }
+}
+// tc : 2 ^ n, n
+// [1, 2]
+/*  
+        []
+     [1]  [2]
+   [1, 2] 
+*/
