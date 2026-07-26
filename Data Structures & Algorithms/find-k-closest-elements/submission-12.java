e88a50ba1c1aedf979026f2a2c4a273d585c3880
YuemengZheng <zheng.yuem@northeastern.edu>
2026-07-25 20:33:26 -0700

    Add: find-k-closest-elements - submission-12

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-12.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        // binary search
+        // find the range of starts
+        // apply binary search to that range
+        // for each mid element:
+        // if target - arr[mid] > arr[mid + k] - target => move r to mid - 1
+        // else move l to mid
+        // return the k sized arr starting from l
+        //  1 2 3 4 5 6， target = 8， k = 2 体会 l < r 和 l <= r的区别
+        int l = 0;
+        int r = arr.length - k;
+        while(l < r) {
+            int mid = l + (r - l) / 2;
+            if(Math.abs(x- arr[mid]) <= Math.abs(x - arr[mid + k])) {
+                r = mid;
+            }else {
+                l = mid + 1;
+            }
+        }
+        List<Integer> res = new ArrayList<>();
+        while(k > 0) {
+            res.add(arr[l++]);
+            k--;
+        }
+        return res;
+    }
+}
