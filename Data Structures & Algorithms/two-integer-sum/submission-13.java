535729e577f156f6ddfaea15a32f20969f2aa73a
YuemengZheng <zheng.yuem@northeastern.edu>
2026-08-20 22:05:51 -0700

    Add: two-integer-sum - submission-13

---
+++ Data Structures & Algorithms/two-integer-sum/submission-13.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        /*
+        The brute force would be checking every pair, which is O(n²). 
+        We can optimize with a HashMap 
+        as we iterate, for each number we check if its complement has already been seen. 
+        If yes, we return both indices. If not, we store the current number and its index. 
+        This gives us O(n) time and O(n) space.
+        */
+        Map<Integer, Integer> seen = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int diff = target - nums[i];
+            if(seen.containsKey(diff)){
+                return new int[]{seen.get(diff), i};
+            }
+            seen.put(nums[i], i);
+        }
+        // this line should never be reached. I'm adding it to satisfy the compiler.
+        return new int[]{-1, -1};
+    }
+}
