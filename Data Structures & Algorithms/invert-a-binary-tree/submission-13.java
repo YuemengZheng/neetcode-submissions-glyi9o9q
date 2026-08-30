7b2011646acb65b2253ac7bdea1cccb7ebff6fde
YuemengZheng <zheng.yuem@northeastern.edu>
2026-08-30 15:07:17 -0700

    Add: invert-a-binary-tree - submission-13

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-13.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        // pre order traversal
+        // base case, when the root is null || it is leaf node return 
+       // for each root, recursively change the position of its children
+       // recursively do the same process
+       dfs(root);
+       return root;
+    }
+    private void dfs(TreeNode root) {
+        if(root == null || root.left == null && root.right == null) return;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        dfs(root.left);
+        dfs(root.right);
+    }
+}
