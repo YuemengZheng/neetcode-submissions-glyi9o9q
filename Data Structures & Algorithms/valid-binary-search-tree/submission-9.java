b19e3078b97701d81ee4e3e0d35e886446065572
YuemengZheng <zheng.yuem@northeastern.edu>
2026-08-30 16:12:33 -0700

    Add: valid-binary-search-tree - submission-9

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-9.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        // try to check each node, if the node is within the right range
+        return dfs(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    private boolean dfs(TreeNode root, int leftBound, int rightBound) {
+        if(root == null) return true;
+
+        if(root.val <= leftBound || root.val >= rightBound) return false;
+        return dfs(root.left, leftBound, root.val) && dfs(root.right, root.val, rightBound); 
+    }
+}
