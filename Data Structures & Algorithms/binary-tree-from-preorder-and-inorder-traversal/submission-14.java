afdb18b57115979e1836dfc33072bccb81b9d208
YuemengZheng <zheng.yuem@northeastern.edu>
2026-08-30 17:36:27 -0700

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-14

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-14.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    // use hashmap to help us find the middle elements in the inorder array in o(1) time
+    Map<Integer, Integer> middleIndex = new HashMap<>();
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        /*
+        Use DFS. 
+        Preorder gives us the root value, 
+        and inorder gives us the left and right partitions. 
+        So we can build the root node and recursively build its left and right children.
+        */
+        for(int i = 0; i < inorder.length; i++){
+            middleIndex.put(inorder[i], i);
+        }
+        return dfs(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+    }
+    private TreeNode dfs(int[] preorder, int preLeft, int preRight, int[] inorder, int inLeft, int inRight){
+        if(preLeft > preRight){
+            return null;
+        }
+
+        // get the middle element
+        int midEle = preorder[preLeft];
+        // build the cur node
+        TreeNode cur = new TreeNode(midEle);
+        // find the index of the middle element in the inorder array
+        int midIndex = middleIndex.get(midEle);
+        // get the left boundary and right boundary of arrays
+        int leftSize = midIndex - inLeft;
+        // recursively build the tree
+        cur.left = dfs(preorder, preLeft + 1, preLeft + leftSize, inorder, inLeft, midIndex - 1);
+        cur.right = dfs(preorder, preLeft + leftSize + 1, preRight, inorder, midIndex + 1, inRight);
+        return cur;
+    }
+}
