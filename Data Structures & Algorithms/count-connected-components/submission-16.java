522a759c72ead7662aa4a1f5214e56b0c3206e6d
YuemengZheng <zheng.yuem@northeastern.edu>
2026-08-30 22:40:15 -0700

    Add: count-connected-components - submission-16

---
+++ Data Structures & Algorithms/count-connected-components/submission-16.java
@@ -0,0 +1,42 @@
+class Solution {
+    class UnionFind {
+        int[] parent;
+        int[] size;
+        public UnionFind(int n) {
+            parent = new int[n];
+            size = new int[n];
+            for(int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+        public int find(int a) {
+            if(a != parent[a]) {
+                parent[a] = find(parent[a]);
+            }
+            return parent[a];
+        }
+        public boolean union(int a, int b) {
+            int rootA = find(a);
+            int rootB = find(b);
+            if(rootA == rootB) return false;
+
+            if(size[rootA] <= size[rootB]) {
+                parent[rootA] = rootB;
+                size[rootB] += size[rootA];
+            }else {
+                parent[rootB] = rootA;
+                size[rootA] += size[rootB];
+            }
+            return true;
+        }
+    }
+    public int countComponents(int n, int[][] edges) {
+        int cnt = n;
+        UnionFind uf = new UnionFind(n);
+        for(int[] e : edges) {
+            if(uf.union(e[0], e[1])) cnt--;
+        }
+        return cnt;
+    }
+}
