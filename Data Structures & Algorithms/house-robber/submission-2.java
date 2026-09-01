4e763c0faaabab804df3e23fad09484bd194b81a
YuemengZheng <zheng.yuem@northeastern.edu>
2026-08-31 21:34:39 -0700

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int rob(int[] nums) {
+        // dp[i] represents the most total money we can rob at the index i
+        // for each i, if i - 1 are not robed -> we have two choose, else we cannot rob
+        // base case 
+        // dp[0] = nums[0]
+        // dp[1] = max(nums[0], nums[1])
+        // dp[i] = rob - dp[i - 1] |  max(not rob dp[i - 1] + nums[i], not robed dp[i - 1]))
+        // finally return max of dp[i] robed or not robbed
+        int prevRob = nums[0];
+        int prevSkip = 0;
+        for(int i = 1; i < nums.length; i++) {
+            int curRob = prevSkip + nums[i];
+            int curSkip = Math.max(prevRob, prevSkip);
+            prevRob = curRob;
+            prevSkip = curSkip;
+        }
+        return Math.max(prevRob, prevSkip);
+    }
+}
